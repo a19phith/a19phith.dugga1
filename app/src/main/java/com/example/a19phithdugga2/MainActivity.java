2cf370543ea5275d49f83f49b429663705e64576
philip johansson thörn <philip2leo@gmail.com>
2020-03-30 17:33:37 +0200

    update hash

---
+++ app/src/main/java/com/example/a19phithdugga2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.a19phithdugga2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Toast.makeText(this, "onCreate() kördes", Toast.LENGTH_LONG).show();
+
+
+    }
+}
